65653bacbf2a3a41a0ef12cf1d062e84c7368b0a
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2022-06-22 11:06:50 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -8,12 +8,18 @@ const char *ssid = ""; // Enter your WiFi name
 const char *password = "";  // Enter WiFi password
 
 // MQTT Broker
-const char *mqtt_broker = "";
-const char *topic = "";
-const char *mqtt_username = "";
-const char *mqtt_password = "";
-const int mqtt_port = 1883;
 
+const char *mqtt_broker = "test.mosquitto.org";
+const char *topic = "";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+int r_goal=48;
+int b_goal=48;
+char red_char[256];
+char blue_char[256];
+String r_goal_srt;
+String b_goal_str;
 
 WiFiClient WIFI;
 PubSubClient MQTT(WIFI);
@@ -29,14 +35,14 @@ void setup() {
   }
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
+  reconnect();
 }
 
 void reconnect() {
   MQTT.setServer(mqtt_broker, mqtt_port);
   MQTT.setCallback(callback);
   while (!MQTT.connected()) {
-   String clientID = "esp8266";
+    String clientID = "esp8266";
     clientID += String(WiFi.macAddress());
     Serial.println("Attempt to connect to MQTT broker");
     if (MQTT.connect(clientID.c_str(), mqtt_username, mqtt_password )) {
@@ -49,8 +55,8 @@ void reconnect() {
       Serial.print(MQTT.state());
     delay(3000);
   }
-
-
+ // Serial.println("MQTT connection succesfull");
+  }
 }
 void callback(char* topic, byte* payload, unsigned int length) {
  
@@ -67,25 +73,47 @@ void callback(char* topic, byte* payload, unsigned int length) {
  
 }
 void loop() {
-
- 
-    
-    
-     if(!digitalRead(BUTTON_RED) == LOW) {
-
-    
+  
+   if(!digitalRead(BUTTON_RED) == LOW) { 
     while(!digitalRead(BUTTON_RED) == LOW) {
-      delay(1000);
-      MQTT.publish(topic, "2");
-     // Serial.println(MQTT.publish(topic, "2"));
+      if(r_goal<=57){
+        delay(1000);
+        r_goal++;
+        Serial.println("RED: ");
+        Serial.printf("%d",r_goal);
+     // MQTT.publish(topic, "2");
+     //   Serial.println(MQTT.publish(topic, r_goal));
+      }else{
+        r_goal=0;
+     //   Serial.println(MQTT.publish(topic, r_goal));
+        Serial.println("RED WINS");
+        Serial.printf("%d",r_goal);
+      }
       }     
     } 
-    if(!digitalRead(BUTTON_BLUE) == LOW) {
-       
+    if(!digitalRead(BUTTON_BLUE) == LOW) { 
       while(!digitalRead(BUTTON_BLUE) == LOW) {
+        if(b_goal<=57){
         delay(1000);
-        MQTT.publish(topic, "1"); 
-       //  Serial.println(MQTT.publish(topic, "1"));
+        b_goal++;
+        Serial.println("BLUE: ");
+        Serial.printf("%d",b_goal); 
+     // MQTT.publish(topic, ""); 
+        b_goal_str=String(b_goal);
+     //   Serial.println(b_goal_str);
+        b_goal_str.toCharArray(blue_char, b_goal_str.length()+1);
+      //  sprintf(blue_char, "%d", b_goal);
+        Serial.printf("%d",blue_char); 
+        Serial.println(MQTT.publish(topic, blue_char));
+       }else{
+        b_goal=0;
+    //    sprintf(blue_char, "%d", b_goal);
+    b_goal_str=String(b_goal);
+        b_goal_str.toCharArray(blue_char, b_goal_str.length()+1);
+        Serial.println(MQTT.publish(topic, blue_char));
+        Serial.println("BLUE WINS");
+        Serial.printf("%d",blue_char);
+       }
         }   
       }  
     
